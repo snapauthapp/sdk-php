12b721495222b88ddcd7ad0747b5978c8c7c8810
Eric Stern <eric@ericstern.com>
2024-08-14 17:34:51 -0700

    Comment and tidy up

--- src/Transport/Response.php
+++ src/Transport/Response.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace SnapAuth\Transport;
 
+/**
+ * This is a vastly simplified version of PSR-7 ResponseInterface. It's used to
+ * avoid forcing an external dependency on clients.
+ */
 final class Response
 {
     /**
-     * @param mixed[] $decodedResponse
+     * @param mixed[] $decoded
      */
     public function __construct(
         public readonly int $code,
